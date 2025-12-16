4a0ffeedf8c72f26d7f3dac81e4ee553a80cbef6
aymen2200 <aymen.djelida@gmail.com>
2025-12-16 18:23:36 +0100

    event loop

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,13 @@
-console.log('Express Tutorial')
+const { readFile, writeFile } = require('fs')
+
+console.log('started a first task')
+// CHECK FILE PATH!!!!
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log(result)
+  console.log('completed first task')
+})
+console.log('starting next task')
